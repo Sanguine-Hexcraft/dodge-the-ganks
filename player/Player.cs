ec06928d9ba0b776d9598cb0218fda78ec5ac381
Sanguine-Hexcraft <156112678+Sanguine-Hexcraft@users.noreply.github.com>
2026-09-10 15:35:28 -0600

    functioning tut game

---
+++ player/Player.cs
@@ -0,0 +1,90 @@
+using Godot;
+
+public partial class Player : Area2D
+{
+    [Signal]
+    public delegate void HitEventHandler();
+
+    // Using the keyword `export` on the speed variable allows us to set it's variable
+    // in the Godot Inspector
+    [Export]
+    public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+
+    public Vector2 ScreenSize; // Size of the game window.
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        // Finds the size of the game window when this node enters the scene tree.
+        ScreenSize = GetViewportRect().Size;
+        Hide();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        var velocity = Vector2.Zero; // The player's movement vector.
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
+
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
+
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+            animatedSprite2D.Play();
+        }
+        else
+        {
+            animatedSprite2D.Stop();
+        }
+
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
+
+        // check movment direction for sprite direction
+        if (velocity.X != 0)
+        {
+            animatedSprite2D.Animation = "walk";
+            animatedSprite2D.FlipV = false;
+            animatedSprite2D.FlipH = velocity.X < 0;
+        }
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        Hide(); // Player disappears after being hit (ganked).
+        EmitSignal(SignalName.Hit);
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D")
+            .SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
+
+    public void Start(Vector2 position)
+    {
+        Position = position;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+    }
+}
